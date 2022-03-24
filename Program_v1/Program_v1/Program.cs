9b157285e4b45d1f1739313c750f0b489fe7ff73
Michał Sawiak <236644@edu.p.lodz.pl>
2022-03-24 23:53:29 +0100

    Wersja koncowa

--- Program_v1/Program_v1/Program.cs
+++ Program_v1/Program_v1/Program.cs
@@ -1,65 +1,67 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-float addition(float a, float b)
+public class Calculator
 {
-    return a + b;
-}
-
-float subtraction(float a, float b)
-{ 
-    return a - b;
-}
-
-float multiplication(float a, float b)
-{
-    return a * b;
-}
-
-float division(float a, float b)
-{
-    return a / b;
-}
-
-bool isZero(float a)
-{
-    if (a == 0) return true;
-    else return false;
-}
-
-int main()
-{
-    Console.WriteLine("Co chcesz zrobic?\n1. Dodawanie\n2. Odejmowanie\n3. Mnozenie\n4. Dzielenie\n");
-
-    string choice = Console.ReadLine();
-    Console.WriteLine("Podaj pierwsza liczbe: ");
-    float first = float.Parse(Console.ReadLine());
-    Console.WriteLine("Podaj druga liczbe: ");
-    float second = float.Parse(Console.ReadLine());
-
-    switch (choice)
+    static float addition(float a, float b)
     {
-        case "1":
-            Console.WriteLine("Dzialanie: " + first + " + " + second + "\nTwoj wynik to: " + addition(first, second));
-            return 0;
-        case "2":
-            Console.WriteLine("Dzialanie: " + first + " - " + second + "\nTwoj wynik to: " + subtraction(first, second));
-            return 0;
-        case "3":
-            Console.WriteLine("Dzialanie: " + first + " * " + second + "\nTwoj wynik to: " + multiplication(first, second));
-            return 0;
-        case "4":
-            if (isZero(second) == true)
-            {
-                Console.WriteLine("Nie mozna dzielic przez zero!\nKoncze program.");
-                return 1;
-            }
-            Console.WriteLine("Dzialanie: " + first + " / " + second + "\nTwoj wynik to: " + division(first, second));
-            return 0;
-        default:
-            Console.WriteLine("Zly wybor.\nKoncze program.");
-            return 1;
+        return a + b;
+    }
+
+    static float subtraction(float a, float b)
+    {
+        return a - b;
+    }
+
+    static float multiplication(float a, float b)
+    {
+        return a * b;
+    }
+
+    static float division(float a, float b)
+    {
+        return a / b;
+    }
+
+    static bool isZero(float a)
+    {
+        if (a == 0) return true;
+        else return false;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Co chcesz zrobic?\n1. Dodawanie\n2. Odejmowanie\n3. Mnozenie\n4. Dzielenie\n");
+
+        string choice = Console.ReadLine();
+        Console.WriteLine("Podaj pierwsza liczbe: ");
+        float first = float.Parse(Console.ReadLine());
+        Console.WriteLine("Podaj druga liczbe: ");
+        float second = float.Parse(Console.ReadLine());
+
+        switch (choice)
+        {
+            case "1":
+                Console.WriteLine("Dzialanie: " + first + " + " + second + "\nTwoj wynik to: " + addition(first, second));
+                break;
+            case "2":
+                Console.WriteLine("Dzialanie: " + first + " - " + second + "\nTwoj wynik to: " + subtraction(first, second));
+                break;
+            case "3":
+                Console.WriteLine("Dzialanie: " + first + " * " + second + "\nTwoj wynik to: " + multiplication(first, second));
+                break;
+            case "4":
+                if (isZero(second) == true)
+                {
+                    Console.WriteLine("Nie mozna dzielic przez zero!\nKoncze program.");
+                    break;
+                }
+                Console.WriteLine("Dzialanie: " + first + " / " + second + "\nTwoj wynik to: " + division(first, second));
+                break;
+            default:
+                Console.WriteLine("Zly wybor.\nKoncze program.");
+                break;
+        }
     }
 }
 
-main();
 
---
+++ Program_v1/TestProgram_v1/TestProgram_v1/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestProgram_v1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
