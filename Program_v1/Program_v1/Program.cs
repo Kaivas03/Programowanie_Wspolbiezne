823f0ffbc81331e98fdd66f8dc83bee76a3749a2
Michał Sawiak <236644@edu.p.lodz.pl>
2022-03-24 23:18:59 +0100

    Poprawka interfejsu

--- Program_v1/Program_v1/Program.cs
+++ Program_v1/Program_v1/Program.cs
@@ -31,19 +31,21 @@ int main()
     Console.WriteLine("Co chcesz zrobic?\n1. Dodawanie\n2. Odejmowanie\n3. Mnozenie\n4. Dzielenie\n");
 
     string choice = Console.ReadLine();
+    Console.WriteLine("Podaj pierwsza liczbe: ");
     float first = float.Parse(Console.ReadLine());
+    Console.WriteLine("Podaj druga liczbe: ");
     float second = float.Parse(Console.ReadLine());
 
     switch (choice)
     {
         case "1":
-            Console.WriteLine(first + " + " + second + "\nTwoj wynik to: " + addition(first, second));
+            Console.WriteLine("Dzialanie: " + first + " + " + second + "\nTwoj wynik to: " + addition(first, second));
             return 0;
         case "2":
-            Console.WriteLine(first + " + " + second + "\nTwoj wynik to: " + subtraction(first, second));
+            Console.WriteLine("Dzialanie: " + first + " - " + second + "\nTwoj wynik to: " + subtraction(first, second));
             return 0;
         case "3":
-            Console.WriteLine(first + " + " + second + "\nTwoj wynik to: " + multiplication(first, second));
+            Console.WriteLine("Dzialanie: " + first + " * " + second + "\nTwoj wynik to: " + multiplication(first, second));
             return 0;
         case "4":
             if (isZero(second) == true)
@@ -51,7 +53,7 @@ int main()
                 Console.WriteLine("Nie mozna dzielic przez zero!\nKoncze program.");
                 return 1;
             }
-            Console.WriteLine(first + " + " + second + "\nTwoj wynik to: " + division(first, second));
+            Console.WriteLine("Dzialanie: " + first + " / " + second + "\nTwoj wynik to: " + division(first, second));
             return 0;
         default:
             Console.WriteLine("Zly wybor.\nKoncze program.");
