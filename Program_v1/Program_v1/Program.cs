362c69a519afd6ffa69ff86a00fe1f05f0fea2dd
Michał Sawiak <236644@edu.p.lodz.pl>
2022-03-24 23:15:28 +0100

    Kod programu

--- Program_v1/Program_v1/Program.cs
+++ Program_v1/Program_v1/Program.cs
@@ -1,2 +1,63 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+float addition(float a, float b)
+{
+    return a + b;
+}
+
+float subtraction(float a, float b)
+{ 
+    return a - b;
+}
+
+float multiplication(float a, float b)
+{
+    return a * b;
+}
+
+float division(float a, float b)
+{
+    return a / b;
+}
+
+bool isZero(float a)
+{
+    if (a == 0) return true;
+    else return false;
+}
+
+int main()
+{
+    Console.WriteLine("Co chcesz zrobic?\n1. Dodawanie\n2. Odejmowanie\n3. Mnozenie\n4. Dzielenie\n");
+
+    string choice = Console.ReadLine();
+    float first = float.Parse(Console.ReadLine());
+    float second = float.Parse(Console.ReadLine());
+
+    switch (choice)
+    {
+        case "1":
+            Console.WriteLine(first + " + " + second + "\nTwoj wynik to: " + addition(first, second));
+            return 0;
+        case "2":
+            Console.WriteLine(first + " + " + second + "\nTwoj wynik to: " + subtraction(first, second));
+            return 0;
+        case "3":
+            Console.WriteLine(first + " + " + second + "\nTwoj wynik to: " + multiplication(first, second));
+            return 0;
+        case "4":
+            if (isZero(second) == true)
+            {
+                Console.WriteLine("Nie mozna dzielic przez zero!\nKoncze program.");
+                return 1;
+            }
+            Console.WriteLine(first + " + " + second + "\nTwoj wynik to: " + division(first, second));
+            return 0;
+        default:
+            Console.WriteLine("Zly wybor.\nKoncze program.");
+            return 1;
+    }
+}
+
+main();
+
