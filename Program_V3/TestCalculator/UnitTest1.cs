75d9cd718a07a2e19729b30554a7818c5427d3ec
Michał Sawiak <236644@edu.p.lodz.pl>
2022-03-31 23:52:24 +0200

    Wprowadzenie testow

--- Program_V3/TestCalculator/UnitTest1.cs
+++ Program_V3/TestCalculator/UnitTest1.cs
@@ -21,14 +21,41 @@ namespace TestCalculator
             Assert.AreEqual(calc.Addition(-1, 1), 0);
             Assert.AreEqual(calc.Addition(44444444, 22222222), 66666666);
 
+            Assert.AreEqual(calc.Multiplication(5, 6), 30);
+            Assert.AreEqual(calc.Multiplication(-1, 1), -1);
+            Assert.AreEqual(calc.Multiplication(4444, 2222), 9874568);
+
+            Assert.AreEqual(calc.Division(6, 6), 1);
+
         }
 
+        [TestMethod]
         public void TestMethod2()
         {
-            Assert.AreEqual(calc.Subtraction(5, 6), 11);
-            Assert.AreEqual(calc.Subtraction(-1, 1), 0);
-            Assert.AreEqual(calc.Subtraction(44444444, 22222222), 66666666);
+
+            Assert.AreEqual(calc.Subtraction(5, 6), -1);
+            Assert.AreEqual(calc.Subtraction(-1, 1), -2);
+            Assert.AreEqual(calc.Subtraction(44444444, 22222222), 22222222);
 
         }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+
+            Assert.AreEqual(calc.Multiplication(5, 6), 30);
+            Assert.AreEqual(calc.Multiplication(-1, 1), -1);
+            Assert.AreEqual(calc.Multiplication(4444, 2222), 9874568);
+
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+
+            Assert.AreEqual(calc.Division(6, 6), 1);
+
+        }
+
     }
 }
