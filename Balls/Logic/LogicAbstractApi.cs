cb6cb474b4c92cd7db8faa790e911c7d787a7703
PGralczyk <92301627+PGralczyk@users.noreply.github.com>
2023-04-22 21:38:38 +0200

    Shift to Data layer
    Changed data location to Data layer

--- Balls/Logic/LogicAbstractApi.cs
+++ Balls/Logic/LogicAbstractApi.cs
@@ -8,9 +8,6 @@ namespace Logic
     public abstract class LogicAbstractApi
     {
         public abstract event EventHandler Update;
-        public abstract int Width { get; }
-        public abstract int Height { get; }
-        internal abstract List<Ball> balls { get; }
         public abstract void CreateBallsList(int count);
         public abstract void UpdateBalls();
         public abstract void Start();
@@ -31,70 +28,40 @@ namespace Logic
     {
         private readonly TimerApi timer;
 
-        public override int Width { get; }
-        public override int Height { get; }
-        internal override List<Ball> balls { get; }
         private DataAbstractApi dataLayer;
         public LogicApi(int width, int height, TimerApi WPFTimer)
         {
-            dataLayer = DataAbstractApi.CreateApi();
-            Width = width;
-            Height = height;
+            dataLayer = DataAbstractApi.CreateApi(width, height);
             timer = WPFTimer;
-            balls = new List<Ball>();
             SetInterval(30);
             timer.Tick += (sender, args) => UpdateBalls();
         }
         public override void CreateBallsList(int count)
         {
-            Random random = new Random();
-            if (count > 0)
-            {
-                for (uint i = 0; i < count; i++)
-                {
-                    int radius = random.Next(20, 40);
-                    int x = random.Next(radius, Width - radius);
-                    int y = random.Next(radius, Height - radius);
-                    int newX = random.Next(radius);
-                    int newY = random.Next(radius);
-                    Ball ball = new Ball(radius, x, y, newX, newY);
-                    balls.Add(ball);
-                }
-            }
-            if (count < 0)
-            {
-                for (int i = count; i < 0; i++)
-                {
-                    if (balls.Count > 0)
-                    {
-                        balls.Remove(balls[balls.Count - 1]);
-                    };
-                }
-            }
+            dataLayer.CreateBallsList(count);
         }
 
         public override event EventHandler Update { add => timer.Tick += value; remove => timer.Tick -= value; }
 
         public override int GetX(int i)
         {
-            return balls[i].X;
+            return dataLayer.GetX(i);
         }
-        public override int GetCount { get => balls.Count; }
+        public override int GetCount => dataLayer.GetCount;
 
         public override int GetY(int i)
         {
-            return balls[i].Y;
+            return dataLayer.GetY(i);
         }
         public override int GetSize(int i)
         {
-            return balls[i].Size;
+            return dataLayer.GetSize(i);
         }
         public override void UpdateBalls()
         {
-            foreach (Ball ball in balls)
+            foreach (Ball ball in dataLayer.GetBallsList())
             {
                 ball.newPosition(600, 480);
-
             }
         }
 
